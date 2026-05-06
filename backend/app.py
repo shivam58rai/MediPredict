855c39b706d4f1b75d972b6ed24b07c4dc78b98d
Shivam Ray <shivam158ray@gmail.com>
2026-05-07 00:23:44 +0530

    added model files

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,8 @@ import pandas as pd
 import numpy as np
 import os
 
-FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "..", "frontend")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.abspath(os.path.join(BASE_DIR, "../frontend"))
 
 app = Flask(__name__)
 CORS(app)
